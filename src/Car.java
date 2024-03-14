be3cd1d895c21394e0e9eb3eb2a53d82ee3132e6
sasha131680 <alexandracky@gmail.com>
2024-03-14 22:51:39 +0200

    mission passed

--- src/Car.java
+++ src/Car.java
@@ -1,14 +1,16 @@
 public class Car {
-    String engineName = "1cd";
+    Engine engine= new Engine();
+
+
     double weight = 1000;
     double maxSpeed = 100;
     int seatsNumber = 5;
     String carName = "toyota";
 
-    // TODO: (Level 2) добавь вместо engineName класс Engine, в котором опиши количество цилиндров, массу двигателя и названия
+
     // TODO: (Level 3) добавь у Engine метод toString(), который будет возвращать строку из всех своих параметров через запятую
     public void talk() {
-        System.out.println("car model:" + carName + ", engine name=" + engineName + ", weight:" + weight +", max speed:"
+        System.out.println("car model:" + carName + ", engine name=" + engine.model + ", weight:" + weight +", max speed:"
         + maxSpeed);
     }
 
---
+++ src/Engine.java
@@ -0,0 +1,6 @@
+public class Engine {
+    int cylinders=4;
+    double weight=90;
+    String model="1cd-ftv";
+
+}
