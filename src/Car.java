f33d4fd7deb37ad7590af9830bb363529c8871e3
sasha131680 <alexandracky@gmail.com>
2024-03-14 22:09:17 +0200

    class car

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,16 @@
 public class Car {
     // TODO: добавь переменные для: вес, макс. скорость, количество мест
+    double weight;
+
+
+    double maxSpeed;
+    int seatsNumber;
+    String carName;
+
+    public void talk() {
+        System.out.println("bipbip");
+    }
+
     // TODO: добавь метод для отображения всех этих полей в консоль в виде: Car: mass = x, speed = y, seats = z
 }
+
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Programmer progger = new Programmer();
-        progger.work();
-        Director director=new Director();
-        director.work();
+       Car car1=new Car();
+       car1.talk();
 
     }
 }
