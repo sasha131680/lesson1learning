86e049c62bff0dcb4a6ae1b407983f150088deb6
2mt <twometrue@gmail.com>
2024-03-14 22:14:25 +0200

    Add TODOs

--- src/Car.java
+++ src/Car.java
@@ -1,16 +1,14 @@
 public class Car {
-    // TODO: добавь переменные для: вес, макс. скорость, количество мест
     double weight;
-
-
     double maxSpeed;
     int seatsNumber;
     String carName;
+    // TODO: добавь переменную с названием двигателя машины
 
     public void talk() {
         System.out.println("bipbip");
     }
 
-    // TODO: добавь метод для отображения всех этих полей в консоль в виде: Car: mass = x, speed = y, seats = z
+    // TODO: добавь метод для отображения всех этих полей в консоль
 }
 
